dfd1c0a2d33dd4523b842b17e41c19ea1aa78b8b
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2023-11-11 13:52:05 +0700

    demo

--- CSE305-Lab/src/cse305/lab/CSE305Lab.java
+++ CSE305-Lab/src/cse305/lab/CSE305Lab.java
@@ -1,20 +1,135 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package cse305.lab;
 
-/**
- *
- * @author USER
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSE305Lab {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       
+
     }
-    
+
+    private static class User {
+
+        long userID;
+        String userFirstName;
+        String userLastName;
+        String userEmail;
+        String password;
+
+        public User(long userID, String userFirstName, String userLastName, String userEmail, String password) {
+            super();
+            this.userID = userID;
+            this.userFirstName = userFirstName;
+            this.userLastName = userLastName;
+            this.userEmail = userEmail;
+            this.password = password;
+        }
+
+        public void register() {
+
+        }
+
+        public void login() {
+
+        }
+
+        public void updateProfile() {
+
+        }
+
+        public void resetPassword() {
+
+        }
+    }
+
+    private static class PropertyOwner {
+
+        List<Property> propertyOwner = new ArrayList<>();
+
+        public void listProperty() {
+
+        }
+
+        public void createProperty() {
+
+        }
+
+        public void udateProperty() {
+
+        }
+
+        public void deleteProperty() {
+
+        }
+    }
+
+    private static class Tenant {
+
+        private List<RetalContract> tanant = new ArrayList<>();
+
+        private void createRentalContract() {
+
+        }
+
+        private void TerminateRentalContract() {
+
+        }
+
+    }
+
+    private static class Property {
+
+        long propertyID;
+        String description;
+        float rentalRate;
+
+        public Property(long propertyID, String description, float rentalRate) {
+
+            this.propertyID = propertyID;
+            this.description = description;
+            this.rentalRate = rentalRate;
+        }
+
+        public void createProperty() {
+
+        }
+
+        public void updateProperty() {
+
+        }
+
+        public void deteleProperty() {
+
+        }
+    }
+
+    private static class RetalContract {
+
+        long contracyID;
+        long propertyID;
+        long tenantID;
+        String startDate;
+        String endDate;
+        float renAmount;
+
+        public RetalContract(long contracyID, long propertyID, long tenantID, String startDate, String endDate,
+                float renAmount) {
+            super();
+            this.contracyID = contracyID;
+            this.propertyID = propertyID;
+            this.tenantID = tenantID;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.renAmount = renAmount;
+        }
+
+        public void createContract() {
+
+        }
+
+        public void terminateContract() {
+
+        }
+    }
+
 }
