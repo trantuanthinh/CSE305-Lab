bd57128b5258dee7b269e3ae8814e346109d3509
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2023-11-11 13:20:20 +0700

    test

---
+++ CSE305-Lab/src/cse305/lab/CSE305Lab.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cse305.lab;
+
+/**
+ *
+ * @author USER
+ */
+public class CSE305Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
